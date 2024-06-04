5e96e0eb436371af171cf455f74af046fe6e4587
Mahdi <m.ghorbi2003@gmail.com>
2024-06-04 10:17:37 +0330

    gusses_game

---
+++ project/gusse_game.py
@@ -0,0 +1,20 @@
+Number = 27
+
+for Effort in range(3):
+    numbers_said=int(input(f"please enter a n umber from 1 to 50: {Effort +1}/3 "))
+    
+    if numbers_said == Number:
+        print("good job! You guessed it right ")
+        break
+    else:
+        if Effort<2:
+            print("Your guess was wrong! try again")
+             
+else :
+    print("you lost! ")
+    
+   
+
+
+
+    
